41e4c1e22a4f23cedb0478222b0f28caad020000
Kingsley Iziegbe <agbesokingsley@yahoo.com>
2025-12-07 07:01:38 +0000

    Sync UI components into root components/ui for Next alias

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,17 +1,1 @@
-import * as React from "react";
-
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-export function Input({ className = "", ...props }: InputProps) {
-  return (
-    <input
-      {...props}
-      className={
-        "border border-gray-300 rounded-md px-3 py-2 w-full focus:ring-2 focus:ring-black " +
-        className
-      }
-    />
-  );
-}
-
-export default Input;
+import React from "react";export function Input(props:React.InputHTMLAttributes<HTMLInputElement>){return <input {...props} className={`border rounded-xl px-3 py-2 outline-none focus:ring-2 focus:ring-black/20 ${props.className||""}`}/>}
