15edf8b685ae82c29f30a75b14ec393fed210960
Kinseso <98305078+Kinseso@users.noreply.github.com>
2025-12-07 08:51:19 +0000

    Update next.config.mjs
    Simplify next.config.mjs (remove webpack alias)

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config) => {
-    // Allow imports like "@/components/..." to point into "src"
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
 };
 
 export default nextConfig;
