874d1aef20d3f155f87f9eff94c536c24515175a
Kingsley Iziegbe <agbesokingsley@yahoo.com>
2026-03-10 01:38:53 +0000

    set dynamic api routes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
-};
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"]
+  })
 
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma
+}
